a6d20b82dc3a541f70fa8e3f7ecdb4c1dcb47499
pilletla <pilletla brahmaiah>
2025-05-21 20:00:51 +0530

    hello see ones

---
+++ 04.variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+echo "plase enter user name::" 
+
+read USERNAME #here USERENAME is variable 
+
+echo "please enter password::" 
+
+read PASSWORD  
+
+echo "username is : $USENAME, password is : $PASSWORD"
