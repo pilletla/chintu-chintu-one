1ff46d71c1042b9ad51259dc06c5224934d49137
pilletla <pilletla brahmaiah>
2025-05-21 19:45:10 +0530

    hello see ones

--- 02.variable.sh
+++ 02.variable.sh
@@ -1,7 +1,10 @@
  
 #!/bin/bash 
 
-echo "Ramesh:: Hello Suresh,How are You?" 
-echo "Suresh:: Hi Ramesh I am Fine" 
-echo "Ramesh:: I am fine too,How is Work" 
-echo "Suresh:: not bad. I am thinking Upgrade Devops"  
+PERSON1=Sachine
+PERSON2=Rahul
+
+echo "$PERSON1:: Hello S$PERSON2,How are You?" 
+echo "$PERSON2:: Hi $PERSON1 I am Fine" 
+echo "$PERSON1:: I am fine too,How is Work" 
+echo "$PERSON1:: not bad. I am thinking Upgrade Devops"  
