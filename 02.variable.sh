1acd20eefa0e27b28c4f183cd1d16298c665830e
pilletla <pilletla brahmaiah>
2025-05-21 19:39:00 +0530

    hello see ones

---
+++ 02.variable.sh
@@ -0,0 +1,7 @@
+ 
+#!/bin/bash 
+
+echo "Ramesh:: Hello Suresh,How are You?" 
+echo "Suresh:: Hi Ramesh I am Fine" 
+echo "Ramesh:: I am fine too,How is Work" 
+echo "Suresh:: not bad. I am thinking Upgrade Devops"  
