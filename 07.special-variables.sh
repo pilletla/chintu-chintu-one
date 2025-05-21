cce9a386c9730c9d49cf7a010fb2164053924d6d
pilletla <pilletla brahmaiah>
2025-05-21 21:27:26 +0530

    hello see ones

---
+++ 07.special-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+echo "All variables: $@"
+
+echo "Number of variables passed: $#" 
+echo "Sccript name: $0" 
+echo "current working directory: $PWD" 
+echo "Home directory of user: $HOME"
